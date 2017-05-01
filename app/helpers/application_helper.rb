ecca9da10968ac7e46779d937cef6fe18d8300c3
Robert Swetts <robert.swetts@gmail.com>
2017-05-01 15:56:09 -0400

    More progress on ch4. I have Ruby experience, so this one isn't too bad

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+	# this is available in ALL views, as it's in appicatyion helper
+	# Hartl ch4, returns full title on a per-page basis
+
+	def full_title(page_title = '')
+		base_title = "Ruby on Rails Tutorial Sample App"
+		
+		if page_title.empty? 				# page title is overwritten if passed in, otherwise blank
+			base_title								# this is an 'Implicit Return'
+		else
+			page_title + " | " + base_title
+		end
+	end
 end
