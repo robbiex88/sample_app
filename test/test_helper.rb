b81f6de5ddffaca4a1b0f8430bb8d96d8bc9de4a
Robert Swetts <robert.swetts@gmail.com>
2017-05-01 09:37:01 -0400

    Completed chapter 3

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,8 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require "minitest/reporters"
+Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
@@ -8,3 +10,18 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
 end
+
+# Commented below out, replaced with above from hartl book
+# essentially this spits colors out to the test terminal window
+# making it easier to read
+
+# ENV['RAILS_ENV'] ||= 'test'
+# require File.expand_path('../../config/environment', __FILE__)
+# require 'rails/test_help'
+
+# class ActiveSupport::TestCase
+#   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+#   fixtures :all
+
+#   # Add more helper methods to be used by all tests here...
+# end
