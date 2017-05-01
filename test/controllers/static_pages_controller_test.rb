93316188710e94b2b917e182a425de08abee6da2
Robert Swetts <robert.swetts@gmail.com>
2017-05-01 09:50:56 -0400

    Completed advanced setup at end of ch3 / specifically, guard and updated .gitignore

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -10,7 +10,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get root" do
   	get root_url
   	assert_response :success
-  	#assert select "title", "Home | #{@base_title}"
+  	assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get home" do
